a6d262d25dc61ea37f590679998deb90d814d49e
tom <bogin7@gmail.com>
2020-03-14 15:55:17 +0200

    port from env

--- js/index.ts
+++ js/index.ts
@@ -1,11 +1,9 @@
-import * as net from "net";
-import { Socket } from "dgram";
 import { Room } from "./Core/Room";
 import { Server } from './Core/server';
 import { MyGameRoom } from './MyGameRoom';
 import { MyGameUser } from './MyGameUser';
 import { RoomFinder } from './Core/RoomFinder';
-
+let port: number = (process.env.PORT && parseInt(process.env.PORT)) || 3002;
 let room = new MyGameRoom();
 export class DefaultRoomFinder extends RoomFinder {
     find(data: any): Room {
@@ -14,5 +12,5 @@ export class DefaultRoomFinder extends RoomFinder {
 }
 let roomFinder = new DefaultRoomFinder();
 
-let server = new Server(3002, (id, socket) => new MyGameUser(id, socket, roomFinder));
+let server = new Server(port, (id, socket) => new MyGameUser(id, socket, roomFinder));
 server.start();
