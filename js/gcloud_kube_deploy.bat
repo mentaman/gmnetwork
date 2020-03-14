95621a4fd185e393a32d1fcf5cd90ddb618d401e
tom <bogin7@gmail.com>
2020-03-14 23:39:49 +0200

    default cluster size - cheaper

--- js/gcloud_kube_deploy.bat
+++ js/gcloud_kube_deploy.bat
@@ -1,4 +1,4 @@
-gcloud container clusters create gmtest-cluster --zone us-west1-a
+gcloud container clusters create gmtest-cluster --zone us-west1-a --machine-type=f1-micro --num-nodes=1 --max-nodes=1
 gcloud container clusters get-credentials gmtest-cluster
 kubectl run gmtest-cluster --image gcr.io/gmnetwork/node-project3 --port 3002
 
