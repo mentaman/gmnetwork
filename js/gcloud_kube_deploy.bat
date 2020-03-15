ad6ee1b8de730c29433789979c1cb9304c2c3143
tom <bogin7@gmail.com>
2020-03-15 03:08:37 +0200

    deploy fix

--- js/gcloud_kube_deploy.bat
+++ js/gcloud_kube_deploy.bat
@@ -1,5 +1,5 @@
-gcloud container clusters create gmtest-cluster --zone us-west1-a --machine-type=f1-micro --num-nodes=1 --max-nodes=1
-gcloud container clusters get-credentials gmtest-cluster
+gcloud container clusters create gmtest-cluster --zone us-west1-a --machine-type=g1-small --num-nodes=1 --max-nodes=1
+gcloud container clusters get-credentials gmtest-cluster --zone us-west1-a
 kubectl run gmtest-cluster --image gcr.io/gmnetwork/node-project3 --port 3002
 
 kubectl expose deployment gmtest-cluster --type "LoadBalancer"
